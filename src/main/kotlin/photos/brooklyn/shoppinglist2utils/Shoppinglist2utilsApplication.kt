1b460504ee746797c68ad97f8558d868691b361b
Brooklyn Photos <guoneng.list@gmail.com>
2018-02-05 19:39:55 -0500

    Initial file setup

---
+++ src/main/kotlin/photos/brooklyn/shoppinglist2utils/Shoppinglist2utilsApplication.kt
@@ -0,0 +1,11 @@
+package photos.brooklyn.shoppinglist2utils
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Shoppinglist2utilsApplication
+
+fun main(args: Array<String>) {
+    runApplication<Shoppinglist2utilsApplication>(*args)
+}
---
+++ src/test/kotlin/photos/brooklyn/shoppinglist2utils/Shoppinglist2utilsApplicationTests.kt
@@ -0,0 +1,16 @@
+package photos.brooklyn.shoppinglist2utils
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Shoppinglist2utilsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
